b503044670f943bb16533218b0485b9ecb9493ca
lyect <lyect1@gmail.com>
2022-10-15 01:16:02 +0700

    integral v2: changed from step to number of intervals

--- integral/main.cpp
+++ integral/main.cpp
@@ -7,23 +7,24 @@
 #include <cmath>
 
 // e^x
-double f(const double &x){
+long double f(const long double &x){
 	return std::exp(x);
 }
 
 // This function returns calculated definite integral of e^x
 //	using trapezoidal rule
-double TrapezoidalRule(const double &a, const double &b, const double &step) {
-	double definite_integral = 0;
+long double TrapezoidalRule(const long double &a, const long double &b, const int &interval_number) {
+	long double definite_integral = 0;
 
-	for (double x = a; x < b; x += step) {
+	long double step = (b - a) / interval_number;
+	for (long double x = a; x < b; x += step) {
 
 		// Bounds of the trapeze
-		double x1 = x;
-		double x2 = x + step;
+		long double x1 = x;
+		long double x2 = x + step;
 
 		// Area of the trapeze
-		double step_trapeze = step * (f(x1) + f(x2)) / 2;
+		long double step_trapeze = step * (f(x1) + f(x2)) / 2;
 
 		definite_integral += step_trapeze;
 	}
@@ -33,18 +34,18 @@ double TrapezoidalRule(const double &a, const double &b, const double &step) {
 
 // This function returns calculated definite integral of e^x
 //	using Simpson's rule
-double SimpsonsRule(const double &a, const double &b, const double &step) {
+long double SimpsonsRule(const long double &a, const long double &b, const int &interval_number) {
+	long double definite_integral = 0;
 
-	double definite_integral = 0;
-
-	for (double x = a; x < b; x += step) {
+	long double step = (b - a) / interval_number;
+	for (long double x = a; x < b; x += step) {
 
 		// Bounds of the segment
-		double x1 = x;
-		double x2 = x + step;
+		long double x1 = x;
+		long double x2 = x + step;
 
 		// Area of that segment
-		double step_segment = step * (f(x1) + 4 * f((x1 + x2) / 2) + f(x2)) / 6;
+		long double step_segment = step * (f(x1) + 4 * f((x1 + x2) / 2) + f(x2)) / 6;
 
 		definite_integral += step_segment;
 	}
@@ -52,69 +53,70 @@ double SimpsonsRule(const double &a, const double &b, const double &step) {
 	return definite_integral;
 }
 
-double get_trapeze_error_power(const double &a, const double &b, const double &step) {
+long double get_trapeze_error_power(const long double &a, const long double &b, const int &interval_number) {
 
-	double true_integral = f(b) - f(a);
+	long double true_integral = f(b) - f(a);
 
-	double full_step_integral = TrapezoidalRule(a, b, step);
-	double half_step_integral = TrapezoidalRule(a, b, step / 2);
+	long double full_step_integral = TrapezoidalRule(a, b, interval_number);
+	long double half_step_integral = TrapezoidalRule(a, b, 2 * interval_number);
 
-	double full_step_error = full_step_integral - true_integral;
-	double half_step_error = half_step_integral - true_integral;
+	long double full_step_error = full_step_integral - true_integral;
+	long double half_step_error = half_step_integral - true_integral;
 
 	return std::log2(full_step_error / half_step_error);
 }
 
-double get_simpson_error_power(const double &a, const double &b, const double &step) {
+long double get_simpson_error_power(const long double &a, const long double &b, const int &interval_number) {
 
-	double true_integral = f(b) - f(a);
+	long double true_integral = f(b) - f(a);
 
-	double full_step_integral = SimpsonsRule(a, b, step);
-	double half_step_integral = SimpsonsRule(a, b, step / 2);
+	long double full_step_integral = SimpsonsRule(a, b, interval_number);
+	long double half_step_integral = SimpsonsRule(a, b, 2 * interval_number);
 
-	double full_step_error = std::abs(full_step_integral - true_integral);
-	double half_step_error = std::abs(half_step_integral - true_integral);
+	long double full_step_error = std::abs(full_step_integral - true_integral);
+	long double half_step_error = std::abs(half_step_integral - true_integral);
 
 	return std::log2(full_step_error / half_step_error);
 }
 
 int main() {
 	
-	double a = 0;
-	double b = 1;
+	long double a = 0;
+	long double b = 10;
 
-	double h1 = 1e-2;
-	double h2 = 1e-3;
+	int interval_number;
 
 	// Set precision of the output
 	std::cout << std::fixed << std::setprecision(9);
 
 	std::cout << "True integral:\n\t" << f(b) - f(a) << "\n";
 
-	std::cout << "Integration from " << a << " to " << b << " with step = 0.01:\n";
+	interval_number = 100;
+	std::cout << "Integration from " << a << " to " << b << " with 100 intervals:\n";
+	std::cout << "\tTrapeze: " << TrapezoidalRule(a, b, interval_number) << "\n";
+	std::cout << "\tSimpson: " << SimpsonsRule(a, b, interval_number) << "\n";
 
-	std::cout << "\tTrapeze: " << TrapezoidalRule(a, b, h1) << "\n";
-	std::cout << "\tSimpson: " << SimpsonsRule(a, b, h1) << "\n";
-
-	std::cout << "Integration from " << a << " to " << b << " with step = 0.001:\n";
-
-	std::cout << "\tTrapeze: " << TrapezoidalRule(a, b, h2) << "\n";
-	std::cout << "\tSimpson: " << SimpsonsRule(a, b, h2) << "\n";
+	interval_number = 1000;
+	std::cout << "Integration from " << a << " to " << b << " with 1000 intervals:\n";
+	std::cout << "\tTrapeze: " << TrapezoidalRule(a, b, interval_number) << "\n";
+	std::cout << "\tSimpson: " << SimpsonsRule(a, b, interval_number) << "\n";
 
 	// Integration steps are changed fro error's power computation.
-	// Because "double" is too imprecise for these calculations.
+	// Because "long double" is too imprecise for these calculations.
 
 	// Trapezoidal rule error
-	std::cout << "Power of the trapezoidal rule error with integration step = 0.1:\n";
-	std::cout << "\t" << get_trapeze_error_power(a, b, 1e-1) << "\n";
-	std::cout << "Power of the trapezoidal rule error with integration step = 0.01:\n";
-	std::cout << "\t" << get_trapeze_error_power(a, b, 1e-2) << "\n";
+	std::cout << "Power of the trapezoidal rule error with 100 intervals:\n";
+	std::cout << "\t" << get_trapeze_error_power(a, b, interval_number) << "\n";
+	std::cout << "Power of the trapezoidal rule error with 1000 intervals:\n";
+	std::cout << "\t" << get_trapeze_error_power(a, b, interval_number) << "\n";
 
 	// Simpson's rule error
-	std::cout << "Power of the Simpson's rule error with integration step = 0.1:\n";
-	std::cout << "\t" << get_simpson_error_power(a, b, 1e-1) << "\n";
-	std::cout << "Power of the Simpson's rule error with integration step = 0.01:\n";
-	std::cout << "\t" << get_simpson_error_power(a, b, 1e-2) << "\n";
+	interval_number = 100;
+	std::cout << "Power of the Simpson's rule error with 100 intervals:\n";
+	std::cout << "\t" << get_simpson_error_power(a, b, interval_number) << "\n";
+	interval_number = 1000;
+	std::cout << "Power of the Simpson's rule error with 1000 intervals:\n";
+	std::cout << "\t" << get_simpson_error_power(a, b, interval_number) << "\n";
 
 	return 0;
 }
